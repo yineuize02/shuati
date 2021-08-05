45ebc0f029f1b476c73724e7a77aa0fc60b87b72
ezeroone02 <ezeroone02@gmail.com>
2021-08-05 22:38:45 +0800

    https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/

---
+++ src/main/java/com/fml/shuati/LengthOfLongestSubstring.java
@@ -0,0 +1,60 @@
+package com.fml.shuati;
+
+import java.util.HashSet;
+
+/** https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/ */
+public class LengthOfLongestSubstring {
+
+  public static void main(String[] args) {
+    //
+
+    var tt = new LengthOfLongestSubstring();
+    int r = 0;
+    r = tt.lengthOfLongestSubstring("abc");
+    System.out.println(r);
+    r = tt.lengthOfLongestSubstring("dvdf");
+    System.out.println(r);
+    r = tt.lengthOfLongestSubstring(" ");
+    System.out.println(r);
+
+    r = tt.lengthOfLongestSubstring("aabaab!bb");
+    System.out.println(r);
+  }
+
+  public int lengthOfLongestSubstring(String s) {
+
+    var set = new HashSet<Character>(s.length());
+    int maxL = 0;
+    int preIndex = 0;
+    for (int i = 0; i < s.length(); ++i) {
+      var c = s.charAt(i);
+      if (set.contains(c)) {
+        if (maxL < set.size()) {
+          maxL = set.size();
+        }
+
+        while (true) {
+          if (preIndex >= i) {
+            break;
+          }
+
+          var preChar = s.charAt(preIndex);
+          set.remove(preChar);
+          preIndex++;
+
+          if (!set.contains(c)) {
+            break;
+          }
+        }
+      }
+
+      set.add(c);
+    }
+
+    if (maxL < set.size()) {
+      maxL = set.size();
+    }
+
+    return maxL;
+  }
+}
