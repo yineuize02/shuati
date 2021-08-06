baa14d035bd323b99a16b50889db61a46dd7a17d
Think <ezeroone@163.com>
2021-08-06 21:22:34 +0800

    https://leetcode-cn.com/problems/maximum-subarray/

---
+++ src/main/java/com/fml/shuati/MaxSubArray.java
@@ -0,0 +1,33 @@
+package com.fml.shuati;
+
+/** https://leetcode-cn.com/problems/maximum-subarray/ */
+public class MaxSubArray {
+
+  public static void main(String[] args) {
+    //
+    var tt = new MaxSubArray();
+    System.out.println(tt.maxSubArray(new int[] {-2, 1, -3, 4, -1, 2, 1, -5, 4}));
+  }
+
+  public int maxSubArray(int[] nums) {
+
+    var length = nums.length;
+    if (length == 1) {
+      return nums[0];
+    }
+
+    int maxSum = nums[0];
+    int curSum = nums[0];
+    for (int i = 1; i < length; ++i) {
+      if (curSum < 0) {
+        curSum = nums[i];
+      } else {
+        curSum += nums[i];
+      }
+
+      maxSum = Math.max(maxSum, curSum);
+    }
+
+    return maxSum;
+  }
+}
